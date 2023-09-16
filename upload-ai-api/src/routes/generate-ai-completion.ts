1861843525b0a8880c0a0859d2baa04b979ab60a
coutbiaafnts <bia.fontes653@gmail.com>
2023-09-16 09:03:31 -0300

    connect backend to frontend

--- upload-ai-api/src/routes/generate-ai-completion.ts
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -1,39 +1,50 @@
-import { FastifyInstance } from "fastify"
-import { prisma } from "../lib/prisma"
-import { openai } from "../lib/openai"
-import { z } from "zod"
+import { FastifyInstance } from "fastify";
+import { streamToResponse, OpenAIStream } from "ai";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+import { z } from "zod";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
-    })
+    });
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
         id: videoId,
-      }
-    })
+      },
+    });
 
     if (!video.transcription) {
-      return reply.status(400).send({ error: 'Video transcription was not generate yet.'})
+      return reply
+        .status(400)
+        .send({ error: "Video transcription was not generate yet." });
     }
 
-    const promptMessage = template.replace('{transcription}', video.transcription)
+    const promptMessage = prompt.replace(
+      "{transcription}",
+      video.transcription
+    );
 
     const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo-16k',
+      model: "gpt-3.5-turbo-16k",
       temperature,
-      messages: [
-        { role: 'user', content: promptMessage },
-      ]
+      messages: [{ role: "user", content: promptMessage }],
+      stream: true,
+    });
+
+    const stream = OpenAIStream(response);
+
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+      }
     })
-
-    return response
-
   });
 }
--- upload-ai-web/src/app.tsx
+++ upload-ai-web/src/app.tsx
@@ -12,8 +12,36 @@ import {
 } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
+import { useCompletion } from "ai/react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  function handlePromptSelected(template: string) {
+    console.log(template);
+  }
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: "http://localhost:3333/ai/complete",
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -39,11 +67,14 @@ export function App() {
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea
               className="resize-none p-4 leading-relaxed"
               placeholder="Resultado gerado pela IA..."
               readOnly
+              value={completion}
             />
           </div>
 
@@ -56,24 +87,14 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="tittle">Título do YouTube</SelectItem>
-                  <SelectItem value="description">
-                    Descrição do YouTube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={setInput} />
             </div>
 
             <Separator />
@@ -97,7 +118,13 @@ export function App() {
 
             <div className="space-y-4">
               <Label>Temperatura</Label>
-              <Slider min={0} max={1} step={0.1} />
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e
                 com possíveis erros.
@@ -106,7 +133,7 @@ export function App() {
 
             <Separator />
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isLoading} type="submit" className="w-full">
               Executar
               <Wand2 className="w-4 h-4 ml-2" />
             </Button>
